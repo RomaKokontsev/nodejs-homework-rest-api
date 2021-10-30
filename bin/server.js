8217c5c24d47e18aaf431152cb5a890dd9430fea
Roma Kokontsev <Romeo42@ukr.net>
2021-10-30 17:58:41 +0300

    add

--- bin/server.js
+++ bin/server.js
@@ -1,14 +1,26 @@
 const app = require('../app');
 const db = require('../model/db');
 const mongoose = require('mongoose');
+const path = require('path');
+const createFolderIsExist = require('../helpers/create-dir');
+require('dotenv').config();
 
 const PORT = process.env.PORT || 3000;
 
 db.then(() => {
-    app.listen(PORT, () => {
-        console.log(`Server running. Use our API on port: ${PORT}`);
-    });
+  app.listen(PORT, async () => {
+    const UPLOAD_DIR = process.env.UPLOAD_DIR;
+    const PUBLIC_DIR = process.env.PUBLIC_DIR;
+    const AVATARS_OF_USERS = path.join(
+      PUBLIC_DIR,
+      process.env.AVATARS_OF_USERS,
+    );
+    await createFolderIsExist(UPLOAD_DIR);
+    await createFolderIsExist(PUBLIC_DIR);
+    await createFolderIsExist(AVATARS_OF_USERS);
+    console.log(`Server running. Use our API on port: ${PORT}`);
+  });
 }).catch(err => {
-    console.log(`Server not running. Error message: ${err.message}`);
-    process.exit(1);
+  console.log(`Server not running. Error message: ${err.message}`);
+  process.exit(1);
 });
